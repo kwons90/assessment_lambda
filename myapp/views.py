1435e3053eb910300e3500e727e5d2d72ef358a4
Kwon <sanghyukkwon90@gmail.com>
2023-04-11 08:05:15 -0400

    changes

--- myapp/views.py
+++ myapp/views.py
@@ -24,10 +24,20 @@ class student:
         self.question_count = len(json["assessment_data"])
         self.total_correct = sum(self.result["accuracy"])
 
+def getImage(url, title):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(str(title)+".jpg", "wb") as f:
+            f.write(response.content)
+
 def generateReport(json_data):
     #initialize the instance of the student
     student1 = student(json_data)
     result = student1.result
+    accu_weight = 1
+    commu_weight = 2
+    under_weight = 2
+    speed = sum(result.speed)
 
     subjectl = result["subject"].tolist()
     accuracyl = result["accuracy"].tolist()
@@ -42,66 +52,87 @@ def generateReport(json_data):
     #set up of the header
     pdf.set_xy(0, 0)
     pdf.set_font('arial', 'B', 12)
-    pdf.image("myapp/Prepbox_logo2.png", x = 15, y = 10, w = 40, h = 0, type = '', link = '')
-    pdf.ln(26)
+    pdf.image(os.path.join("/Users/skwon/Documents/PDFmaker/Prepbox_logo2.png"), x = 15, y = 10, w = 40, h = 0, type = '', link = '')
+    pdf.ln(25)
     #title
-    pdf.cell(190, 10, "Grade Level Assessment Test Result for "+ student1.name, 0, 1, 'C')
+    pdf.cell(190, 10, "Grade Level Assessment Test Result for "+ student1.name, 0, 1,"C")
     #body
     pdf.set_font('arial', 'B', 9)
     #Summary part
-    pdf.ln(3)
+    pdf.ln(5)
     pdf.cell(7)
     pdf.set_fill_color(91,155,213)
     pdf.set_text_color(255,255,255)
-    pdf.cell(80, 5.5, "Level Test Summary", 0, 1, "L", fill=True)
+    pdf.cell(80, 5, "Level Test Summary", 0, 1, "L", fill=True)
     pdf.set_fill_color(0,0,0)
     pdf.set_text_color(0,0,0)
     pdf.set_font('arial', '', 9)
     pdf.cell(7)
-    pdf.cell(50, 5.5, "Student Name:", 0, 0, 'L')
-    pdf.cell(30, 5.5, student1.name, 0, 1, 'R')
+    pdf.cell(50, 5, "Student Name:", 0, 0, 'L')
+    pdf.cell(30, 5, student1.name, 0, 1, 'R')
     pdf.cell(7)
-    pdf.cell(50, 5.5, "Student Grade:", 0, 0, 'L')
-    pdf.cell(30, 5.5, str(student1.grade), 0, 1, 'R')
+    pdf.cell(50, 5, "Student Grade:", 0, 0, 'L')
+    pdf.cell(30, 5, str(student1.grade), 0, 1, 'R')
     pdf.cell(7)
-    pdf.cell(50, 5.5, "Test Date:", 0, 0, 'L')
-    pdf.cell(30, 5.5, str(student1.date), 0, 1, 'R')
+    pdf.cell(50, 5, "Test Date:", 0, 0, 'L')
+    pdf.cell(30, 5, str(student1.date), 0, 1, 'R')
     pdf.cell(7)
-    pdf.cell(50, 5.5, "Tester:", 0, 0, 'L')
-    pdf.cell(30, 5.5, str(student1.tester), 0, 1, 'R')
+    pdf.cell(50, 5, "Tester:", 0, 0, 'L')
+    pdf.cell(30, 5, str(student1.tester), 0, 1, 'R')
     pdf.ln(5)
     #The results
     pdf.set_font('arial', 'B', 9)
     pdf.set_fill_color(91,155,213)
     pdf.set_text_color(255,255,255)
     pdf.cell(7)
-    pdf.cell(90, 5.5, "Subject", 0, 0, "L", fill=True)
-    pdf.cell(30, 5.5, "Accuracy", 0, 0, "R", fill=True)
-    pdf.cell(30, 5.5, "Proof of work", 0, 0, "R", fill=True)
-    pdf.cell(30, 5.5, "Understanding", 0, 1, "R", fill=True)
+    pdf.cell(90, 5, "Subject", 0, 0, "L", fill=True)
+    pdf.cell(30, 5, "Accuracy", 0, 0, "R", fill=True)
+    pdf.cell(30, 5, "Proof of work", 0, 0, "R", fill=True)
+    pdf.cell(30, 5, "Understanding", 0, 1, "R", fill=True)
     pdf.set_fill_color(0,0,0)
     pdf.set_text_color(0,0,0)
     pdf.set_font('arial', '', 10)
     for i in range(0,student1.question_count):
         pdf.cell(7)
-        pdf.cell(90, 5.5, "Q"+str(i+1)+ ".: "+subjectl[i], 0, 0, "L")
-        pdf.cell(30, 5.5, str(accuracyl[i]), 0, 0, "R")
-        pdf.cell(30, 5.5, str(communicationl[i]), 0, 0, "R")
-        pdf.cell(30, 5.5, str(understandingl[i]), 0, 1, "R")
+        pdf.cell(90, 5, "Q"+str(i+1)+ ".: "+subjectl[i], 0, 0, "L")
+        pdf.cell(30, 5, str(accuracyl[i]), 0, 0, "R")
+        pdf.cell(30, 5, str(communicationl[i]), 0, 0, "R")
+        pdf.cell(30, 5, str(understandingl[i]), 0, 1, "R")
     pdf.cell(7)
     pdf.set_font('arial', 'B', 9)
-    pdf.cell(90, 5.5, "Total Score", "T", 0, "L")
-    pdf.cell(30, 5.5, str(sum(accuracyl)), "T", 0, "R")
-    pdf.cell(30, 5.5, str(sum(communicationl)), "T", 0, "R")
-    pdf.cell(30, 5.5, str(sum(understandingl)), "T", 1, "R")
+    pdf.cell(90, 5, "Total Score", "T", 0, "L")
+    pdf.cell(30, 5, str(sum(accuracyl)), "T", 0, "R")
+    pdf.cell(30, 5, str(sum(communicationl)), "T", 0, "R")
+    pdf.cell(30, 5, str(sum(understandingl)), "T", 1, "R")
+
+    pdf.cell(7)
+    pdf.set_font('arial', '', 9)
+    pdf.cell(90, 5, "Weight", 0, 0, "L")
+    pdf.cell(30, 5, str(accu_weight), 0, 0, "R")
+    pdf.cell(30, 5, str(commu_weight), 0, 0, "R")
+    pdf.cell(30, 5, str(under_weight), 0, 1, "R")
+
+    pdf.cell(7)
+    pdf.set_font('arial', 'B', 9)
+    pdf.cell(90, 5, "Total Weighted Score", "T", 0, "L")
+    pdf.cell(30, 5, str(sum(accuracyl)*accu_weight), "T", 0, "R")
+    pdf.cell(30, 5, str(sum(communicationl)*commu_weight), "T", 0, "R")
+    pdf.cell(30, 5, str(sum(understandingl)*under_weight), "T", 1, "R")
+
+    weighted_score = sum(accuracyl)*accu_weight + sum(communicationl)*commu_weight + sum(understandingl)*under_weight
+    total_score = (accu_weight+commu_weight+under_weight)*student1.question_count
+    weighted_score_percent = int((weighted_score/total_score)*100)
+    accu_score = int(((sum(accuracyl)*accu_weight) / (student1.question_count*accu_weight))*100)
+    commu_score = int(((sum(communicationl)*commu_weight) / (student1.question_count*commu_weight))*100)
+    under_score = int(((sum(understandingl)*under_weight) / (student1.question_count*under_weight))*100)
 
     pdf.ln(5)
     pdf.cell(7)
-    pdf.cell(140, 5.5, "Total Combined Score", 0, 0, "L")
-    pdf.cell(40, 5.5, str(82), 0, 1, "R")
+    pdf.cell(140, 5, "Total Combined Score", 0, 0, "L")
+    pdf.cell(40, 5, str(weighted_score_percent)+"%", 0, 1, "R")
     pdf.cell(7)
-    pdf.cell(140, 5.5, "Time Spent", 0, 0, "L")
-    pdf.cell(40, 5.5, str("120 minutes"), 0, 1, "R")
+    pdf.cell(140, 5, "Time Spent", 0, 0, "L")
+    pdf.cell(40, 5, str(sum(timel))+" minutes", 0, 1, "R")
 
     pdf.ln(5)
     pdf.set_font('arial', 'B', 9)
@@ -110,31 +141,43 @@ def generateReport(json_data):
     pdf.cell(7)
     pdf.cell(25, 5.5, " Category", 1, 0, "L", fill=True)
     pdf.cell(95, 5.5, str("Description"), 1, 0, "C", fill=True)
-    pdf.cell(20, 5.5, str("Score"), 1, 0, "C", fill=True)
-    pdf.cell(40, 5.5, str("Recommendation"), 1, 1, "C", fill=True)
+    pdf.cell(25, 5.5, str("Score"), 1, 0, "C", fill=True)
+    pdf.cell(35, 5.5, str("Recommendation"), 1, 1, "C", fill=True)
     pdf.set_font('arial', '', 9)
     pdf.set_fill_color(255,255,255)
     pdf.set_text_color(0,0,0)
     pdf.cell(7)
     pdf.cell(25, 5.5, " Accuracy", 1, 0, "L")
     pdf.cell(95, 5.5, "Whether the answer was right or wrong", 1, 0, "C")
-    pdf.cell(20, 5.5, str("Sco123re"), 1, 0, "C")
-    pdf.cell(40, 5.5, str("Needs improvement"), 1, 1, "C")
+    pdf.cell(25, 5.5, str(accu_score)+"% "+str(sum(accuracyl)*accu_weight)+" / "+str(student1.question_count*accu_weight), 1, 0, "C")
+    if accu_score < 90:
+        pdf.cell(35, 5.5, str("Needs improvement"), 1, 1, "C")
+    else:
+        pdf.cell(35, 5.5, str("Satisfactory"), 1, 1, "C")
     pdf.cell(7)
     pdf.cell(25, 5.5, " Speed", 1, 0, "L")
     pdf.cell(95, 5.5, "How the long student took to complete", 1, 0, "C")
-    pdf.cell(20, 5.5, str("123"), 1, 0, "C")
-    pdf.cell(40, 5.5, str("Needs improvement"), 1, 1, "C")
+    pdf.cell(25, 5.5, str(speed)+" minutes", 1, 0, "C")
+    if speed < 90:
+        pdf.cell(35, 5.5, str("Needs improvement"), 1, 1, "C")
+    else:
+        pdf.cell(35, 5.5, str("Satisfactory"), 1, 1, "C")
     pdf.cell(7)
     pdf.cell(25, 5.5, " Communication", 1, 0, "L")
     pdf.cell(95, 5.5, "How clean the student shows proof of work and steps", 1, 0, "C")
-    pdf.cell(20, 5.5, str("123"), 1, 0, "C")
-    pdf.cell(40, 5.5, str("Needs improvement"), 1, 1, "C")
+    pdf.cell(25, 5.5, str(commu_score)+"% "+str(sum(communicationl)*commu_weight)+" / "+str(student1.question_count*commu_weight), 1, 0, "C")
+    if commu_score > 90:
+        pdf.cell(35, 5.5, str("Satisfactory"), 1, 1, "C")
+    else:
+        pdf.cell(35, 5.5, str("Needs improvement"), 1, 1, "C")
     pdf.cell(7)
     pdf.cell(25, 5.5, " Understanding", 1, 0, "L")
     pdf.cell(95, 5.5, "How much the student's work demonstrate clear understanding", 1, 0, "C")
-    pdf.cell(20, 5.5, str("213123"), 1, 0, "C")
-    pdf.cell(40, 5.5, str("Needs improvement"), 1, 1, "C")
+    pdf.cell(25, 5.5, str(under_score)+"% "+str(sum(understandingl)*under_weight)+" / "+str(student1.question_count*under_weight), 1, 0, "C")
+    if under_score > 90:
+        pdf.cell(35, 5.5, str("Satisfactory"), 1, 1, "C")
+    else:
+        pdf.cell(35, 5.5, str("Needs improvement"), 1, 1, "C")
     pdf.ln(5)
     pdf.cell(7)
     pdf.set_fill_color(91,155,213)
@@ -145,23 +188,13 @@ def generateReport(json_data):
     pdf.set_text_color(0,0,0)
     pdf.set_font('arial', '', 9)
     pdf.cell(155, 30, student1.comment, 1, 0, "C")
-    for url in imgl:
+
+    for i in range(0,student1.question_count):
         # Download the image
-        response = requests.get(url)
-        if response.status_code == 200:
-            # Add a new page to the PDF
-            pdf.add_page()
-
-            # Get the dimensions of the downloaded image
-            with open("image.jpg", "wb") as f:
-                f.write(response.content)
-
-        # Add the image to the PDF
-        pdf.image("image.jpg", x=15, y=15, w=150, h=150)
-
-    else:
-        # Print an error message if the image can't be downloaded
-        print(f"Failed to download image from {url}: {response.status_code}")
+        getImage(imgl[i], i)
+            
+        pdf.add_page()
+        pdf.image(str(i)+".jpg", x=15, y=15, w=150, h=150)
 
 
     pdf_data = pdf.output(dest='S').encode('latin1')
